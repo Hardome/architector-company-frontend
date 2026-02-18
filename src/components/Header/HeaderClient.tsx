8b1b7087c12aa26b3320b1f075311a864f76bf1d
Semyon Bedrin <semyonbedrin@gmail.com>
2026-02-18 22:26:06 +0500

    phone

--- src/components/Header/HeaderClient.tsx
+++ src/components/Header/HeaderClient.tsx
@@ -8,7 +8,7 @@ import ChooseHouseButton from '@/components/ChooseHouseButton';
 import {Button} from '@/components/ui/button';
 import {P} from '@/components/ui/typography';
 import useScrollToSection from '@/hooks/useScrollToSection';
-import {MENU_ITEMS} from '@/lib/constants';
+import {COMPANY, MENU_ITEMS} from '@/lib/constants';
 
 import MobileMenu from './MobileMenu';
 
@@ -63,14 +63,14 @@ export default function HeaderClient() {
             </nav>
             <div className={'flex items-center gap-4'}>
               <a
-                href={'tel:+74951234567'}
+                href={COMPANY.phoneHref}
                 className={
                   'hidden lg:flex items-center gap-2 text-white hover:text-gray-300 ' +
                   'transition-colors text-lg font-medium'
                 }
               >
                 <Phone size={18} />
-                <P>{'+7 (495) 123-45-67'}</P>
+                <P>{COMPANY.phone}</P>
               </a>
               <ChooseHouseButton size={'lg'} className={'hidden lg:flex rounded-full'} />
               <Button
